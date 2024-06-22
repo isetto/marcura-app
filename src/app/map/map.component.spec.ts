a5d51a4209ba08d3c5aebe5289a3d335ec493ad8
Adrian <fak.adrian@gmail.com>
2024-06-22 13:54:14 +0200

    add map component

---
+++ src/app/map/map.component.spec.ts
@@ -0,0 +1,83 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MapComponent } from './map.component';
+import { MatCardModule } from '@angular/material/card';
+import { Subject } from 'rxjs';
+import { RouteData } from '../models/route.model';
+import { RouteDataProviderService } from '../services/route-data-provider.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+describe('MapComponent', () => {
+  let component: MapComponent;
+  let fixture: ComponentFixture<MapComponent>;
+  let mockRouteDataProviderService: jasmine.SpyObj<RouteDataProviderService>;
+  let selectedRouteSubject: Subject<RouteData>;
+
+  const mockRouteData: RouteData = {
+    route_id: 1,
+    from_port: 'Port A',
+    to_port: 'Port B',
+    leg_duration: 123,
+    points: [
+      { longitude: 0, latitude: 0, timestamp: 1629988800000, speed: 10 },
+      { longitude: 1, latitude: 1, timestamp: 1629992400000, speed: 15 },
+    ],
+  };
+
+  beforeEach(async () => {
+    selectedRouteSubject = new Subject<RouteData>();
+
+    mockRouteDataProviderService = jasmine.createSpyObj('RouteDataProviderService', ['selectRoute']);
+    mockRouteDataProviderService.selectedRoute$ = selectedRouteSubject.asObservable();
+
+    await TestBed.configureTestingModule({
+      imports: [
+        MatCardModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [MapComponent],
+      providers: [
+        { provide: RouteDataProviderService, useValue: mockRouteDataProviderService }
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MapComponent);
+    fixture.detectChanges();
+    component = fixture.componentInstance;
+
+    const mapContainer = document.createElement('div');
+    mapContainer.id = 'map';
+    document.body.appendChild(mapContainer);
+  });
+
+
+  afterEach(() => {
+    component.ngOnDestroy();
+    let mapContainer = document.getElementById('map');
+    if (mapContainer) {
+      mapContainer.remove();
+      mapContainer = null
+    }
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should subscribe to selectedRoute$ on ngOnInit and call showRoute', () => {
+    const showRouteSpy = spyOn<any>(component, 'showRoute').and.callThrough();
+    component.ngOnInit();
+    selectedRouteSubject.next(mockRouteData);
+    expect(showRouteSpy).toHaveBeenCalledWith(mockRouteData);
+  });
+
+  it('should unsubscribe from routeSubscription on destroy', () => {
+    component.ngOnInit();
+    component.ngOnDestroy();
+    expect(component['routeSubscription'].closed).toBeTruthy();
+  });
+});
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,82 @@
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import { RouteData } from '../models/route.model';
+import { RouteDataProviderService } from '../services/route-data-provider.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements AfterViewInit, OnInit, OnDestroy  {
+  public map!: L.Map;
+  private routeSubscription!: Subscription;
+  private polylines: L.Polyline[] = [];
+
+  constructor(private csvParserService: RouteDataProviderService) {}
+
+  public ngOnInit(): void {
+    this.routeSubscription = this.csvParserService.selectedRoute$.subscribe((route) => {
+      if (route) {
+        this.clearRoute();
+        this.showRoute(route);
+      }
+    });
+  }
+
+  public ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3,
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '© OpenStreetMap contributors',
+      detectRetina: true,
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  private showRoute(route: RouteData): void {
+    const localPolylines = [];
+    for (let i = 0; i < route.points.length - 1; i++) {
+      const p1 = route.points[i];
+      const p2 = route.points[i + 1];
+      const color = this.getColorForSpeed(p1.speed);
+      const segment = L.polyline([[p1.latitude, p1.longitude], [p2.latitude, p2.longitude]], { color });
+      localPolylines.push(segment);
+      segment.addTo(this.map);
+    }
+    this.polylines = localPolylines;
+    this.map.fitBounds(L.featureGroup(localPolylines).getBounds());
+  }
+
+  private clearRoute(): void {
+    if (this.polylines) {
+      this.polylines.forEach((segment) => this.map.removeLayer(segment));
+      this.polylines = [];
+    }
+  }
+
+  private getColorForSpeed(speed: number): string {
+    if (speed < 5) return 'red';
+    if (speed < 10) return 'orange';
+    if (speed < 15) return 'yellow';
+    if (speed < 20) return 'green';
+    return 'blue';
+  }
+
+  public ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+}
